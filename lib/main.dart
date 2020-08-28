014ae5be9266664c20086601168b765e007af65b
unknown <abdelhakim@univ-tiaret.dz>
2020-08-28 13:17:15 +0100

    We got the job done right

--- lib/main.dart
+++ lib/main.dart
@@ -3,115 +3,144 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MyApp());
 }
-
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  ScrollController _scrollController;
 
-  void _incrementCounter() {
+  List allSlides = [
+    {'slideName': 'slideOne', 'selected': false},
+    {'slideName': 'slideTwo', 'selected': false},
+    {'slideName': 'slideThree', 'selected': false},
+    {'slideName': 'slideThree', 'selected': false},
+    {'slideName': 'slideThree', 'selected': false}
+   
+  ];
+
+  var selectedSlide;
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController = ScrollController();
+    _scrollController.addListener(changeSelector);
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      selectedSlide = allSlides[0];
+      selectedSlide['selected'] = true;
+    });
+  }
+
+  changeSelector() {
+    var maxScrollVal = _scrollController.position.maxScrollExtent;
+
+    var divisor = (maxScrollVal / allSlides.length) + 20;
+
+    var scrollValue = _scrollController.offset.round();
+    var slideValue = (scrollValue / divisor).round();
+
+    var currentSlide = allSlides.indexWhere((slide) => slide['selected']);
+
+    setState(() {
+      allSlides[currentSlide]['selected'] = false;
+      selectedSlide = allSlides[slideValue];
+      selectedSlide['selected'] = true;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+        appBar: AppBar(
+          title: Text('Scroll Effects'),
+          centerTitle: true,
+        ),
+        body: Row(
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+            SizedBox(width: 15.0),
+            Container(
+              width: MediaQuery.of(context).size.width / 3,
+              child: ListView(
+                  children: allSlides.map((element) {
+                return getTitles(element);
+              }).toList()),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+            SizedBox(width: 10.0),
+            Container(
+              width: (MediaQuery.of(context).size.width / 3) * 2 - 25.0,
+              child: ListView(
+                controller: _scrollController,
+                children: allSlides.map((element) {
+                  return getCards(element);
+                }).toList(),
+              ),
+            )
           ],
+        ));
+  }
+
+  Widget getCards(slide) {
+    return Padding(
+      padding: EdgeInsets.only(top: 15.0, right: 10.0),
+      child: Container(
+        decoration: BoxDecoration(
+          border: Border.all(
+              color: Colors.black, style: BorderStyle.solid, width: 1.0),
+        ),
+        height: 200.0,
+        width: 125.0,
+        child: Center(
+          child: Text(
+            slide['slideName'],
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  scrollToSlide(inputSlide) {
+    var whichSlide = allSlides
+        .indexWhere((slide) => slide['slideName'] == inputSlide['slideName']);
+
+    var maxScrollValue = _scrollController.position.maxScrollExtent;
+
+    var divisor = (maxScrollValue / allSlides.length) + 20;
+
+    var scrollToValue = whichSlide * divisor;
+
+    _scrollController.animateTo(scrollToValue,
+        curve: Curves.easeIn, duration: Duration(milliseconds: 1000));
+  }
+
+  Widget getTitles(slide) {
+    return InkWell(
+      onTap: () {
+        scrollToSlide(slide);
+      },
+      child: Padding(
+        padding: EdgeInsets.only(top: 15.0),
+        child: Text(
+          slide['slideName'],
+          style: TextStyle(
+              fontWeight:
+                  slide['selected'] ? FontWeight.bold : FontWeight.normal,
+              fontSize: 17.0),
+        ),
+      ),
     );
   }
 }
